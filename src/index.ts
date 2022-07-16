03618b9b0955d6add600fb50b41c2825199191ff
Artur <82472015+benyaar@users.noreply.github.com>
2022-07-16 23:12:20 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -27,28 +27,29 @@ app.get('/bloggers', (req:Request, res:Response) => {
        res.send(bloggers)
    }
 })
-// app.post('/videos', (req:Request, res:Response) => {
-//     let title = req.body.title
-//     if (!title || typeof title !== 'string' || !title.trim() || title.length > 40){
-//         res.status(400).send({
-//             errorsMessages: [
-//             {
-//                 message: "Incorrect title",
-//                 field: "title"
-//             }
-//         ]
-//     })
-//         return
-//     }
-//
-//    const newVideo = {
-//        id: +(new Date()),
-//        title: req.body.title,
-//        author:"sss"
-//    }
-//    videos.push(newVideo)
-//     res.status(201).send(newVideo)
-// })
+app.post('/bloggers', (req:Request, res:Response) => {
+     let name = req.body.name && req.body.youtubeUrl
+     
+     if (!name || typeof name !== 'string' || !name.trim() || name.length > 40){
+         res.status(400).send({
+             errorsMessages: [
+             {
+                 message: "Incorrect title",
+                 field: "title"
+             }
+         ]
+     })
+         return
+     }
+
+    const newBloggers = {
+        id: +(new Date()),
+        name: req.body.name,
+        youtubeUrl: req.body.youtubeUrl
+    }
+    bloggers.push(newBloggers)
+    res.status(201).send(newBloggers)
+ })
 //
 // app.get('/videos/:id', (req:Request, res:Response) => {
 //     let video = videos.find(v => v.id === +req.params.id)
