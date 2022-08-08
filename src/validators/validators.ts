ffd610fe802f9ae2f5d09caf78032d426f97fdf1
Artur <82472015+benyaar@users.noreply.github.com>
2022-08-08 09:21:10 +0300

    Update validators.ts

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -5,5 +5,5 @@ export const urlValidation = body('youtubeUrl').isURL().trim().isLength({min: 10
 export const titleValidation = body('title').trim().isLength({min: 1, max: 30})
 export const shortDescriptionValidation = body('shortDescription').trim().isLength({min: 1, max: 100})
 export const contentValidation = body('content').trim().isLength({min: 1, max: 1000})
-export const loginValidation = body('login').trim().isLength({min: 1, max: 1000})
+export const loginValidation = body('login').trim().isLength({min: 3, max: 1000})
 export const passwordValidation = body('password').trim().isLength({min: 1, max: 20})
