46b4d663e541afba7a409f5650cde27a81c2e654
Artur <82472015+benyaar@users.noreply.github.com>
2022-08-21 11:47:17 +0300

    Update deleteAll-router.ts

--- src/routers/deleteAll-router.ts
+++ src/routers/deleteAll-router.ts
@@ -3,7 +3,7 @@ import {deleteService} from "../domain/delete-service";
 
 export const deleteAllRouter = Router({})
 
-deleteAllRouter.post('/all-data',
+deleteAllRouter.delete('/all-data',
     async (req:Request, res:Response) =>{
         await deleteService.deleteAll()
         res.sendStatus(204)
