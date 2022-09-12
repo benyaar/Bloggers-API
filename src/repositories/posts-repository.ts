942b52ad69bc4d6ddd815be3ae379d3bc76f39e2
Artur <82472015+benyaar@users.noreply.github.com>
2022-09-12 18:42:42 +0300

    Update posts-repository.ts

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -1,12 +1,16 @@
 import { postsModal, PostsType} from "./db";
-
+const options = {
+    _id:0,
+    __v:0,
+    postId:0
+}
 
 export const postsRepository = {
     async findPosts(pageSize:number, pageNumber:number) {
-        return  postsModal.find({}, {_id:0, __v:0}).skip((pageNumber-1)*pageSize).limit(pageSize)
+        return  postsModal.find({}, options).skip((pageNumber-1)*pageSize).limit(pageSize)
     },
     async findPostById(id: string) {
-        return  postsModal.findOne({id: id}, {_id:0, __v:0})
+        return  postsModal.findOne({id: id}, options)
     },
     async createPost(newPosts: PostsType) {
         await postsModal.insertMany(newPosts)
@@ -33,7 +37,7 @@ export const postsRepository = {
         return  postsModal.count({})
     },
     async findBloggersPost(pageSize:number, pageNumber:number, bloggerId: string){
-        return  postsModal.find({bloggerId: bloggerId},  {_id:0, __v:0}).skip((pageNumber-1)*pageSize).limit(pageSize)
+        return  postsModal.find({bloggerId: bloggerId}, options).skip((pageNumber-1)*pageSize).limit(pageSize)
     },
     async getCountBloggerId(bloggerId: string) {
         return  postsModal.count({bloggerId: bloggerId})
